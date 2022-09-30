80a138dcb66c81817373a56277f33d107ba5213d
Alexander Protasov <wakko@acmelabs.spb.ru>
2022-09-30 21:24:37 +0300

    Исправляем запуск gunicorn

--- misc/Makefile.django.mk
+++ misc/Makefile.django.mk
@@ -4,8 +4,8 @@
 run-django:  ## Run django application with debugger
 	$(ACTIVATE) && python manage.py runserver 0.0.0.0:8000
 
-.PHONY: run-django-db-migrate
-run-django-db-migrate:  ## Run django migrate
+.PHONY: run-django-migrate
+run-django-migrate:  ## Run django migrate
 	$(ACTIVATE) && python manage.py migrate
 
 .PHONY: run-django-supervisord
