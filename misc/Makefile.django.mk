f96b82cf12c92a5ec986c74ff7cc565d4dfcd07f
Alexander Protasov <wakko@acmelabs.spb.ru>
2022-09-30 21:12:07 +0300

    Запуск Django через gunicorn и supervisor

--- misc/Makefile.django.mk
+++ misc/Makefile.django.mk
@@ -4,6 +4,10 @@
 run-django:  ## Run django application with debugger
 	$(ACTIVATE) && python manage.py runserver 0.0.0.0:8000
 
-.PHONY: run-django-migrate
-run-django-migrate:  ## Run django migrate
+.PHONY: run-django-db-migrate
+run-django-db-migrate:  ## Run django migrate
 	$(ACTIVATE) && python manage.py migrate
+
+.PHONY: run-django-supervisord
+run-django-supervisord:  ## Run flask application with supervisord
+	$(ACTIVATE) && supervisord -c "$(MISC)/supervisord.docker.conf"
