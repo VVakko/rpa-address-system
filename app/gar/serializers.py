6e187db86d09d2366b18c20d63122a8294f2a2f7
Alexander Protasov <wakko@acmelabs.spb.ru>
2022-10-30 22:54:10 +0300

    Added &limit= feature

--- app/gar/serializers.py
+++ app/gar/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import serializers
+from rest_framework import pagination, serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +12,11 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:  # pylint: disable=too-few-public-methods
         model = Group
         fields = ['url', 'name']
+
+
+class DefaultResultsSetPagination(pagination.PageNumberPagination):
+    """
+    Redefinition of PageNumberPagination for limit feature.
+    """
+    page_size_query_param = 'limit'
+    max_page_size = 10000
--- app/settings.py
+++ app/settings.py
@@ -64,7 +64,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'app.gar.serializers.DefaultResultsSetPagination',
     'PAGE_SIZE': 20,
 }
 if not DEBUG:
